9e5acad1799b7a4e84feea00b1226f2085f684eb
Jiezhong Qiu <jiezhongqiu@outlook.com>
2019-03-21 13:42:23 +0800

    update job script

--- run_mpnn.sh
+++ run_mpnn.sh
@@ -4,9 +4,11 @@ set -x
 for target in mu alpha homo lumo gap r2 zpve U0 U H G Cv
 do
     echo $target
-    mkdir -p checkpoint/$target
+    chk=checkpoint/QM9/$target
+    mkdir -p $chk
     CUDA_VISIBLE_DEVICES=0 python main.py -l $target \
         --lr 0.00013 --lr-decay 0.5 \
-        --resume checkpoint/$target \
-        --batch-size 20 --log-interval 1000 |& tee $target.log
+        --resume $chk \
+        --batch-size 20 --log-interval 200 \
+        --epochs 200 |& tee update_$target.log
 done
